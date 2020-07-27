1bf8618cbe453ebb48760e57b8710c55495e5c03
Erick Christian <erickchristian48@gmail.com>
2020-07-27 22:12:45 +0700

    Added StrictMode and App component

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,2 +1,11 @@
-import '../styles/style.css'
-const img = require('../images/Inalillahi.png')
+import * as React from 'react'
+import { render } from 'react-dom'
+
+import { App } from './App'
+
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
