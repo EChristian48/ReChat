6a3a9550a449fdf970d7dca8ad6fa3e588128c80
Erick Christian <erickchristian48@gmail.com>
2020-07-30 16:49:50 +0700

    Imported many new elements

--- src/scripts/App.tsx
+++ src/scripts/App.tsx
@@ -1,26 +1,35 @@
 import * as React from 'react'
 import { CssBaseline } from '@material-ui/core'
 
-import { HashRouter as Router, Switch, Route } from 'react-router-dom'
-
-import { useCurrentUser } from './hooks/useCurrentUser'
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 import { LoginPage } from './pages/LoginPage'
+import { useAuth } from './hooks/useAuth'
+import { CheckAuth } from './components/CheckAuth'
+import { LogoutPage } from './pages/LogoutPage'
 
 const App: React.FC = () => {
-  const user = useCurrentUser()
+  const user = useAuth()
 
   return (
     <CssBaseline>
       <Router>
         <Switch>
+          {user && <Redirect from='/login' to='/home' />}
+
           <Route path='/login'>
             <LoginPage />
           </Route>
 
-          <Route path='/home'>Home</Route>
+          <Route path='/home'>
+            <CheckAuth user={user}>Home</CheckAuth>
+          </Route>
 
-          <Route path='/logout'>Logout</Route>
+          <Route path='/logout'>
+            <CheckAuth user={user}>
+              <LogoutPage />
+            </CheckAuth>
+          </Route>
         </Switch>
       </Router>
     </CssBaseline>
