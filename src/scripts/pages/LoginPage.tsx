ea8dac6e7195c9520c384023bd21f88c0089c49e
Erick Christian <erickchristian48@gmail.com>
2020-07-30 16:49:20 +0700

    Created a basic Login Page

--- src/scripts/pages/LoginPage.tsx
+++ src/scripts/pages/LoginPage.tsx
@@ -1,18 +1,43 @@
 import * as React from 'react'
 
+import { Button, Grid, Container, Icon } from '@material-ui/core'
+
 import * as firebase from 'firebase/app'
 import 'firebase/auth'
 
-class LoginPage extends React.Component {
+type LoginPageState = {
+  isLoading: boolean
+}
+
+class LoginPage extends React.Component<{}, LoginPageState> {
+  login = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithRedirect(provider)
+  }
+
   async componentDidMount() {
     await firebase.auth().getRedirectResult()
-    console.log('asd')
   }
+
   render() {
     return (
-      <div>
-        <div>asda</div>
-      </div>
+      <Container>
+        <Grid
+          container
+          alignItems='center'
+          justify='center'
+          className='full-height'>
+          <Grid item>
+            <Button
+              onClick={this.login}
+              variant='contained'
+              color='primary'
+              endIcon={<Icon className='fab fa-google' />}>
+              Login With
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
     )
   }
 }
