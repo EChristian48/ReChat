230140834744ae728650ad244938d6e29930e638
Erick Christian <erickchristian48@gmail.com>
2020-07-30 10:57:10 +0700

    useAuth is now useCurrentUser

--- src/scripts/hooks/useAuth.tsx
+++ src/scripts/hooks/useAuth.tsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react'
+
 import * as firebase from 'firebase/app'
 import 'firebase/auth'
 
-const useAuth = () => {}
+type LoggedInStatus = firebase.User | null | 'loading'
+
+const useAuth = () => {
+  const [user, setUser] = useState<LoggedInStatus>('loading')
+
+  useEffect(() => {
+    return firebase.auth().onAuthStateChanged(user => {
+      setUser(user)
+    })
+  })
+
+  return user
+}
 
 export { useAuth }
