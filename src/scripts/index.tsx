0df07edb27c38179626b17d834cd3af78ad9d740
Erick Christian <erickchristian48@gmail.com>
2020-08-02 19:13:38 +0700

    Added config for firebase emulator

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -4,12 +4,20 @@ import { render } from 'react-dom'
 import { App } from './App'
 
 import * as firebase from 'firebase/app'
+import 'firebase/functions'
 import { firebaseConfig } from './firebaseConfig'
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import '../styles/utils.css'
 
 firebase.initializeApp(firebaseConfig)
+
+// DEV ONLY
+firebase.firestore().settings({
+  host: 'localhost:8080',
+  ssl: false,
+})
+firebase.functions().useFunctionsEmulator('http://localhost:5001')
 
 render(
   <React.StrictMode>
