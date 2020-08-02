2a88659f13921d62f9c54406ecd8a00bd93acd82
Erick Christian <erickchristian48@gmail.com>
2020-08-02 14:20:18 +0700

    Done mickup

--- src/scripts/components/ChatListItem.tsx
+++ src/scripts/components/ChatListItem.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react'
 
-const ChatListItem: React.FC = () => {
+import {
+  ListItem,
+  ListItemText,
+  Divider,
+  ListItemAvatar,
+  Avatar,
+} from '@material-ui/core'
+
+type ChatListItemProps = {
+  avatar?: string
+  name: string
+  lastText?: string
+}
+
+const ChatListItem: React.FC<ChatListItemProps> = props => {
   return (
     <>
-      <div></div>
+      <ListItem component='a' href='sujono'>
+        <ListItemAvatar>
+          <Avatar src={props.avatar} />
+        </ListItemAvatar>
+        <ListItemText primary={props.name} secondary={props.lastText} />
+      </ListItem>
+      <Divider />
     </>
   )
 }
