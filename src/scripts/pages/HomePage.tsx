91c1e0e6610e375b990b4c2995f573e6903514d8
Erick Christian <erickchristian48@gmail.com>
2020-08-03 23:35:29 +0700

    Half done mockup I guess

--- src/scripts/pages/HomePage.tsx
+++ src/scripts/pages/HomePage.tsx
@@ -1,27 +1,69 @@
 import * as React from 'react'
 
-import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  List,
+  Box,
+} from '@material-ui/core'
 
-type HomePageState = {}
+import { ChatListItem } from '../../scripts/components/ChatListItem'
+import { AddChatFab } from '../components/AddChatFab'
+import { AddChatDialog } from '../components/AddChatDialog'
+type HomePageState = {
+  isDialogOpen: boolean
+}
 
 class HomePage extends React.Component<{}, HomePageState> {
+  state: HomePageState = {
+    isDialogOpen: false,
+  }
+
+  closeDialog = () => {
+    this.setState({
+      isDialogOpen: false,
+    })
+  }
+
+  openDialog = () => {
+    this.setState({
+      isDialogOpen: true,
+    })
+  }
+
   logout = () => {
     location.href = '/#/logout'
   }
 
   render() {
     return (
-      <AppBar>
-        <Toolbar>
-          <Typography style={{ flexGrow: 1 }} variant='h6'>
-            ReChat
-          </Typography>
+      <Box>
+        <AppBar position='static'>
+          <Toolbar>
+            <Typography style={{ flexGrow: 1 }} variant='h6'>
+              ReChat
+            </Typography>
 
-          <Button variant='outlined' color='inherit' onClick={this.logout}>
-            Logout
-          </Button>
-        </Toolbar>
-      </AppBar>
+            <Button variant='outlined' color='inherit' onClick={this.logout}>
+              Logout
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        <List>
+          <ChatListItem name='Henri Sujono' lastText='Halo aku ganteng' />
+        </List>
+
+        <AddChatFab onClick={this.openDialog} />
+
+        <AddChatDialog
+          open={this.state.isDialogOpen}
+          onClose={this.closeDialog}
+          onConfirm={this.closeDialog}
+        />
+      </Box>
     )
   }
 }
