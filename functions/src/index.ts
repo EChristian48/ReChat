71a2bce18fc99aec123f9fd8e95cd7e6211c61b3
Erick Christian <erickchristian48@gmail.com>
2020-08-04 00:00:27 +0700

    Done some code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,24 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import { User } from './databaseSchema'
+
+admin.initializeApp()
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info('Hello logs!', { structuredData: true })
+  response.send('Hello from Firebase!')
+})
+
+const createUserDocument = functions.auth.user().onCreate(user => {
+  const userDocRef = admin.firestore().doc(`users/${user.uid}`)
+  const userData: User = {
+    name: user.displayName,
+    photoUrl: user.photoURL,
+  }
+})
+
+export { createUserDocument }
