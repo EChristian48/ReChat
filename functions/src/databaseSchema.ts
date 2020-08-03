8f2950808e6d6b4fddb96cd9347c198a997b377b
Erick Christian <erickchristian48@gmail.com>
2020-08-03 23:36:09 +0700

    Added databaseSchema to the functions folder

---
+++ functions/src/databaseSchema.ts
@@ -0,0 +1,14 @@
+interface User {
+  name: string
+  photoUrl: string
+}
+
+interface ChatSession {
+  users: [string, string]
+}
+
+interface Message {
+  sent: string | Date
+  content: string
+  sender: string
+}
